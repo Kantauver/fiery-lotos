8d9a9f1b611efcef12fef33465e937247804ebbb
Kantauver <avakara@gmail.com>
2019-03-26 01:31:14 +0100

    main html-layout, sidebar-menu

--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -7,5 +7,24 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
+  isLeftMenuVisible = true;
+  isSchedeMenuOpened = true;
+  isRegistrazioneMenuOpened = true;
+  isReportMenuOpened = true;
 
+  toggleLeftMenu() {
+    this.isLeftMenuVisible = !this.isLeftMenuVisible;
+  }
+
+  toggleSchedeMenuArrow() {
+      this.isSchedeMenuOpened = !this.isSchedeMenuOpened;
+  }
+
+  toggleRegistrazioneMenuArrow() {
+      this.isRegistrazioneMenuOpened = !this.isRegistrazioneMenuOpened;
+  }
+
+  toggleReportMenuArrow() {
+      this.isReportMenuOpened = !this.isReportMenuOpened;
+  }
 }
