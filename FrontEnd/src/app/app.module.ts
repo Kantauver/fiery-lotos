14c7c016c9155127276f0e1c5feca2d1b5d8781d
Oleg Kravchenko <oleg.kravchenko@lightcode.net>
2019-05-01 14:17:47 +0200

    loader service

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -1,19 +1,31 @@
+// modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { DxMenuModule } from 'devextreme-angular';
+import { DxLoadPanelModule } from 'devextreme-angular/ui/load-panel';
+
+// services
+import { LoaderService } from './services/loader/loader.service';
+
+// components
 import { AppComponent } from './app.component';
+import { LoaderComponent } from './services/loader/loader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    DxMenuModule
+    DxMenuModule,
+    DxLoadPanelModule
   ],
-  providers: [],
+  providers: [
+    LoaderService
+  ],
   bootstrap: [AppComponent]
 })
 
